0412f81be21891baa16d28ec853184dfc5ea560a
Yusuf Ozaslan <yusufozaslan2000@gmail.com>
2022-08-16 22:54:41 +0300

    Reviewed admin control

---
+++ Cake_Cafe/Cake_Cafe/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cake_Cafe.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string ProductName { get; set; }
+
+        public string Photo { get; set; }
+
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")] public Category Category { get; set; }
+    }
+}
