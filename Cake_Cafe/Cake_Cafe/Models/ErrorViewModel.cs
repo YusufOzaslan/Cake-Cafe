467f0b315a567f907f2ea46ac27df59a091d9391
Yusuf Ozaslan <yusufozaslan2000@gmail.com>
2022-08-16 10:29:35 +0300

    initial commit

---
+++ Cake_Cafe/Cake_Cafe/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Cake_Cafe.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
