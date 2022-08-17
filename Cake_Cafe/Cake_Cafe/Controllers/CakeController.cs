6ede415641d59bb6e1cd92b6846b08fa0869cdd4
Yusuf Ozaslan <yusufozaslan2000@gmail.com>
2022-08-17 11:07:41 +0300

    New Product and Revision

--- Cake_Cafe/Cake_Cafe/Controllers/CakeController.cs
+++ Cake_Cafe/Cake_Cafe/Controllers/CakeController.cs
@@ -3,14 +3,24 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Cake_Cafe.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace Cake_Cafe.Controllers
 {
     public class CakeController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        public CakeController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
+        }
+        
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Product;
+            return View(await applicationDbContext.ToListAsync());
         }
     }
 }
