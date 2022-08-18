82490e389caf91e4e9ce197334c0f6973160b954
Yusuf Ozaslan <yusufozaslan2000@gmail.com>
2022-08-18 19:51:44 +0300

    revision

---
+++ Cake_Cafe/Cake_Cafe/Models/Person.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cake_Cafe.Models
+{
+    public class Person : IdentityUser
+    {
+        public string Ad { get; set; }
+
+        public string Soyad { get; set; }
+
+        public Role Role { get; set; }
+    }
+    public enum Role
+    {
+        admin,
+        customer
+    }
+}
