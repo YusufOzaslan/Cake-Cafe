8d5038c482aa68f7a95ebeacce6ae1e47738edbb
Yusuf Ozaslan <yusufozaslan2000@gmail.com>
2022-08-23 22:45:41 +0300

    Revision

---
+++ Cake_Cafe/Cake_Cafe/Data/Migrations/20220823181600_Role.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Cake_Cafe.Data.Migrations
+{
+    public partial class Role : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
